c664fb3e8cad17a72aad6301f71d961abc27c879
steveo1485 <stevennugent@gmail.com>
2013-12-31 16:47:05 -0800

    Create @given_date and @num_to_add instance variables, create #given_date_string? to check if passed date is a string, add tests for #given_date_string?

--- models/day.rb
+++ models/day.rb
@@ -1,3 +1,11 @@
 class Day
 
+  def initialize(date_string, number)
+    @given_date = date_string
+    @num_to_add = number
+  end
+
+  def given_date_string?
+    @given_date.is_a?(String)
+  end
 end
--- spec/day_spec.rb
+++ spec/day_spec.rb
@@ -2,10 +2,23 @@ require 'spec_helper'
 
 describe Day do
   before(:each) do
-    @test_day = Day.new
+    @test_day = Day.new('1/1', 10)
   end
 
-  it "should be a Day" do
-    @test_day.should be_a Day
+  describe "on initialization" do
+    it "should be a Day" do
+      @test_day.should be_a Day
+    end
+  end
+
+  describe "#given_date_string?" do
+    it "should return true if @given_date is a string" do
+      expect(@test_day.given_date_string?).to eq(true)
+    end
+
+    it "should return false if @given_date is not a string" do
+      not_string = Day.new(11, 10)
+      expect(not_string.given_date_string?).to eq(false)
+    end
   end
 end
