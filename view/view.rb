6f298232de15f6a9e9df80a9e0f9c6a08351624a
steveo1485 <stevennugent@gmail.com>
2014-01-01 23:05:36 -0800

    Add return statements to View methods

--- view/view.rb
+++ view/view.rb
@@ -1,7 +1,7 @@
 class View
   def self.get_start_date
     puts "Please enter string of start date in [m]m/[d]d format:"
-    start_date = STDIN.gets.chomp
+    return start_date = STDIN.gets.chomp
   end
 
   def self.start_date_error
@@ -10,7 +10,7 @@ class View
 
   def self.get_number_of_days
     puts "Please enter a number of days to add:"
-    number_of_days = STDIN.gets.chomp
+    return number_of_days = STDIN.gets.chomp
   end
 
   def self.number_error
