e21411884940f9ec5519bd46f7757168427c5bd9
steveo1485 <stevennugent@gmail.com>
2014-01-02 14:23:58 -0800

    Update tests to include nested context instead of describe

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Calendar do
-  describe "::days_in_month" do
+  context "::days_in_month" do
     it "should return an integer for the number of days in a passed month" do
       expect(Calendar.days_in_month(1)).to eq(31)
     end
@@ -11,7 +11,7 @@ describe Calendar do
     end
   end
 
-  describe "::generate_calendars" do
+  context "::generate_calendars" do
     it "should return an array containing hashes of calendars covering given days" do
       expect(Calendar.generate_calendars(1, 70)).to eq([{ 1 => 31, 2 => 28, 3 => 31 }])
       expect(Calendar.generate_calendars(12, 32)).to eq([{ 12 => 31 }, { 1 => 31}])
--- spec/day_spec.rb
+++ spec/day_spec.rb
@@ -5,31 +5,31 @@ describe Day do
     @test_day = Day.new('01/01', 10)
   end
 
-  describe "on initialization" do
+  context "on initialization" do
     it "should be a Day" do
       expect(@test_day).to be_a(Day)
     end
   end
 
-  describe "#get_day" do
+  context "#get_day" do
     it "should return an integer of given day" do
       expect(@test_day.get_day).to eq(1)
     end
   end
 
-  describe "#get_month" do
+  context "#get_month" do
     it "should return an integer of given month" do
       expect(@test_day.get_month).to eq(1)
     end
   end
 
-  describe "#calculate_day" do
+  context "#calculate_day" do
     it "should return an array containing the new date" do
       expect(@test_day.calculate_day(10, [{ 1 => 31 }])).to eq([1, 10])
     end
   end
 
-  describe "#total_days" do
+  context "#total_days" do
     it "should return the sum of the start day and the days to add" do
       expect(@test_day.total_days(10, 10)).to eq(20)
     end
