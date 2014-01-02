0d7a3cb1fe05a4b2c953a533ac435b71e459778f
steveo1485 <stevennugent@gmail.com>
2014-01-01 16:02:23 -0800

    Add get_number_of_days method and test for View, update View methods with string to print to screen

--- spec/view/view_spec.rb
+++ spec/view/view_spec.rb
@@ -8,4 +8,11 @@ describe View do
       expect(View.get_start_date).to eq('01/01')
     end
   end
+
+  describe "::get_number_of_days" do
+    it "should ask for a number of days to add" do
+      STDIN.should_receive(:gets).and_return(10)
+      expect(View.get_number_of_days).to eq(10)
+    end
+  end
 end
