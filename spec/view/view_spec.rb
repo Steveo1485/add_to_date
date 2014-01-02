b9945f57b814ac87bc33880c0f8841b0f622463a
steveo1485 <stevennugent@gmail.com>
2014-01-01 17:13:41 -0800

    Create View methods to return error messages, tests for methods

--- spec/view/view_spec.rb
+++ spec/view/view_spec.rb
@@ -9,10 +9,22 @@ describe View do
     end
   end
 
+  describe "::start_date_error" do
+    it "should return the error message" do
+      expect(View.start_date_error).to eq("Please try again. Be sure the date is in [m]m/[d]d format.")
+    end
+  end
+
   describe "::get_number_of_days" do
     it "should ask for a number of days to add" do
       STDIN.should_receive(:gets).and_return(10)
       expect(View.get_number_of_days).to eq(10)
+    end
+  end
+
+  describe "::number_error" do
+    it "should return the error message" do
+      expect(View.number_error).to eq("Please try again. Make sure your number is an integer and not a float.")
     end
   end
 
--- view/view.rb
+++ view/view.rb
@@ -4,9 +4,17 @@ class View
     start_date = STDIN.gets.chomp
   end
 
+  def self.start_date_error
+    return "Please try again. Be sure the date is in [m]m/[d]d format."
+  end
+
   def self.get_number_of_days
     puts "Please enter a number of days to add:"
     number_of_days = STDIN.gets
+  end
+
+  def self.number_error
+    return "Please try again. Make sure your number is an integer and not a float."
   end
 
   def self.new_date(month, day)
